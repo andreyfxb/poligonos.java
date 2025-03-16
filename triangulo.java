ae6710ee6f4ed60a226431faba86fd5f89b74faf
andreyfxb <andrey1001005@gmail.com>
2025-03-16 19:55:02 -0300

    triangulo.java

---
+++ triangulo.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Triangulo {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Informe o valor de A:");
+        int a = ler.nextInt();
+
+        System.out.println("Informe o valor de B:");
+        int b = ler.nextInt();
+
+        System.out.println("Informe o valor de C:");
+        int c = ler.nextInt();
+
+        if (a + b + c != 180) {
+            System.out.println("Não é um triângulo");
+        } else if (a == b && b == c) {
+            System.out.println("Triângulo Equilátero");
+        } else if (a != b && b != c && a != c) {
+            System.out.println("Triângulo Escaleno");
+        } else {
+            System.out.println("Triângulo Isósceles");
+        }
+    }
+}
