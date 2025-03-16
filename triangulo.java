ed7a063c7c0becae39146701ee05de82d6938787
andreyfxb <andrey1001005@gmail.com>
2025-03-16 20:49:54 -0300

    Update triangulo.java

--- triangulo.java
+++ triangulo.java
@@ -7,7 +7,7 @@ public class TRIANGULO {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Leitura dos lados do triângulo
+        
         System.out.print("Digite o valor do lado A: ");
         int A = scanner.nextInt();
         System.out.print("Digite o valor do lado B: ");
@@ -15,11 +15,11 @@ public class TRIANGULO {
         System.out.print("Digite o valor do lado C: ");
         int C = scanner.nextInt();
 
-        // Verifica se os valores formam um triângulo
+        
         if (A >= (B + C) || B >= (A + C) || C >= (A + B)) {
             System.out.println("Não é um triângulo.");
         } else {
-            // Identifica o tipo de triângulo
+          
             if (A == B && B == C) {
                 System.out.println("Triângulo Equilátero (Todos os lados são iguais).");
             } else if (A == B || A == C || B == C) {
